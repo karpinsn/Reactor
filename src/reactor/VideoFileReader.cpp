b9e2008074198d3e507ed63e8cd923f7d06cb7e6
Nik Karpinsky <karpinsn@iastate.edu>
2012-01-09 12:33:40 -0600

    First commit,

---
+++ src/reactor/VideoFileReader.cpp
@@ -0,0 +1,10 @@
+/*
+ *  VideoFileReader.cpp
+ *  Holoencoder
+ *
+ *  Created by Nikolaus Karpinsky on 01/09/12.
+ *  Copyright 2012 ISU. All rights reserved.
+ *
+ */
+
+#include "VideoFileReader.h"
---
+++ src/reactor/VideoFileReader.h
@@ -0,0 +1,27 @@
+/*
+ Filename:		VideoFileReader.h
+ Author:		Nikolaus Karpinsky
+ Date Created:	01/09/12
+ Last Edited:	01/09/12
+ 
+ Revision Log:
+ 01/09/12 - Nik Karpinsky - Original creation.
+ */
+
+#ifndef _VIDEO_FILE_READER_H_
+#define _VIDEO_FILE_READER_H_
+
+#include <iostream>
+#include <string>
+#include <stdarg.h>
+
+using namespace std;
+
+namespace reactor
+{
+	class VideoFileReader
+	{
+	};
+}
+
+#endif	// _VIDEO_FILE_READER_H_
