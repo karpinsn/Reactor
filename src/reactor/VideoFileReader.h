81f92d24623ef19d3d5cb99053c097c66de040f9
Nik Karpinsky <karpinsn@iastate.edu>
2012-01-09 13:30:39 -0600

    Got Reactor to compile on vista

--- src/reactor/VideoFileReader.h
+++ src/reactor/VideoFileReader.h
@@ -11,9 +11,16 @@
 #ifndef _VIDEO_FILE_READER_H_
 #define _VIDEO_FILE_READER_H_
 
-#include <iostream>
+//	FFmpeg Includes
+#define __STDC_CONSTANT_MACROS
+extern "C"
+{
+  #include <libavcodec/avcodec.h>
+  #include <libavformat/avformat.h>
+  #include <libswscale/swscale.h>
+}
+
 #include <string>
-#include <stdarg.h>
 
 using namespace std;
 
